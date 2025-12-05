e8b82d2d31335d53cd096469b3120c61c12175cb
Kundan <kidjha79902@gmail.com>
2025-12-05 23:47:22 +0530

    app.js fixed

--- ProductManagement Api/wwwroot/app.js
+++ ProductManagement Api/wwwroot/app.js
@@ -3,6 +3,7 @@
 app.controller("productCtrl", function ($scope, $http) {
 
     const baseUrl = "https://localhost:7128/api/Items";
+
     $scope.items = [];
     $scope.showForm = false;
     $scope.isEdit = false;
@@ -30,7 +31,6 @@ app.controller("productCtrl", function ($scope, $http) {
         $scope.form = {};
         $scope.clearMessage();
     };
-
     $scope.editItem = function (item) {
         $scope.showForm = true;
         $scope.isEdit = true;
@@ -47,34 +47,44 @@ app.controller("productCtrl", function ($scope, $http) {
                 $scope.loadItems();
             }, () => $scope.showMessage("Error deleting item", false));
     };
+
     $scope.saveItem = function () {
-        if (!$scope.form.itemCode || !$scope.form.description ||
-            !$scope.form.sellingPrice || !$scope.form.costPrice) {
+
+        if (
+            !$scope.form.itemCode ||
+            !$scope.form.description ||
+            !$scope.form.sellingPrice ||
+            !$scope.form.costPrice
+        ) {
             $scope.showMessage("Please fill all required fields!", false);
             return;
         }
 
         if ($scope.form.photoFile) {
             const file = $scope.form.photoFile;
+
             if (!file.type.startsWith("image/")) {
                 $scope.showMessage("Only image files allowed!", false);
                 return;
             }
+
             if (file.size > 3 * 1024 * 1024) {
                 $scope.showMessage("Image size must be less than 3MB!", false);
                 return;
             }
         }
+
         if ($scope.isEdit) {
             $http.put(baseUrl + "/EditItem", $scope.form)
-                .then(() => {
+                .then((res) => {
                     $scope.showMessage(res.data.message || "Item updated successfully!", true);
                     $scope.loadItems();
                     $scope.cancelForm();
                 }, () => $scope.showMessage("Error updating item", false));
-        } else {
+        }
+        else {
             $http.post(baseUrl + "/AddItem", $scope.form)
-                .then(() => {
+                .then((res) => {
                     $scope.showMessage(res.data.message || "Item added successfully!", true);
                     $scope.loadItems();
                     $scope.cancelForm();
@@ -88,28 +98,32 @@ app.controller("productCtrl", function ($scope, $http) {
     };
     $scope.onFileChange = function (element) {
         const file = element.files[0];
+
         if (file) {
             const reader = new FileReader();
+
             reader.onload = function (e) {
                 $scope.$apply(function () {
-                    $scope.form.photoBase64 = e.target.result.split(",")[1]; 
-                    $scope.form.photoFile = file; 
+                    $scope.form.photoBase64 = e.target.result.split(",")[1];
+                    $scope.form.photoFile = file;
                 });
             };
+
             reader.readAsDataURL(file);
         }
     };
     $scope.showMessage = function (msg, isSuccess) {
         $scope.message = msg;
         $scope.success = isSuccess;
+
         setTimeout(() => {
-            $scope.$apply(function () { $scope.message = ""; });
+            $scope.$apply(function () {
+                $scope.message = "";
+            });
         }, 4000);
     };
-
     $scope.clearMessage = function () {
         $scope.message = "";
     };
-
     $scope.init();
 });
